f338e6a49b844c12a496963b03ef9e1c79561d74
Mike Yang <yangy25@rpi.edu>
2021-09-22 13:08:50 -0400

    script commit

--- WEB-INF/classes/UploadServlet.java
+++ WEB-INF/classes/UploadServlet.java
@@ -47,7 +47,7 @@ public class UploadServlet extends HttpServlet {
         if (!fileSaveDir.exists()) {
             fileSaveDir.mkdir();
         }
-        print_out(request);
+        // print_out(request);
         for (Part part : request.getParts()) {
             String fileName = extractFileName(part);
             // refines the fileName in case it is an absolute path
