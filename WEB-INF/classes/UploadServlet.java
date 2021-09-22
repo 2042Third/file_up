7639bb545e33c5efd029ed1864d8c8b3b97b6a6d
Mike Yang <yangy25@rpi.edu>
2021-09-22 11:19:55 -0400

    script commit

--- WEB-INF/classes/UploadServlet.java
+++ WEB-INF/classes/UploadServlet.java
@@ -19,7 +19,17 @@ public class UploadServlet extends HttpServlet {
      * the web application directory.
      */
     private static final String SAVE_DIR = "download";
-     
+    
+    public static Map<String, String> splitQuery(String query) throws UnsupportedEncodingException {
+      Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+      String[] pairs = query.split("&");
+      for (String pair : pairs) {
+        System.out.println(pair);
+          int idx = pair.indexOf("=");
+          query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+      }
+      return query_pairs;
+  } 
     /**
      * handles file upload
      */
