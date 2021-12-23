c67138f6c56baeef1f9531c1caad481d74253271
mike yang <18604713262@163.com>
2021-12-23 11:21:29 -0500

    script commit

--- WEB-INF/classes/UploadServlet.java
+++ WEB-INF/classes/UploadServlet.java
@@ -7,14 +7,14 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.io.InputStream;
 import java.io.BufferedReader;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.InputStreamReader;
-import javax.servlet.http.Part;
+import jakarta.servlet.http.Part;
 @WebServlet("/UploadServlet")
 @MultipartConfig(fileSizeThreshold=1024*1024*2, // 2MB
                  maxFileSize=1024*1024*10,      // 10MB
